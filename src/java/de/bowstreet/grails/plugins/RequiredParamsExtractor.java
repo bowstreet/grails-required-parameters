749f253cea298996b5e8c5357ad5810fe997a8aa
Matthias Wenz <mat@bowstreet.de>
2012-12-27 14:25:01 +0100

    Include Logger

--- src/java/de/bowstreet/grails/plugins/RequiredParamsExtractor.java
+++ src/java/de/bowstreet/grails/plugins/RequiredParamsExtractor.java
@@ -4,9 +4,16 @@ import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.codehaus.groovy.grails.commons.GrailsClass;
 
 public class RequiredParamsExtractor {
+    
+    public static final Logger log;
+    
+    static {
+        log = Logger.getLogger(RequiredParamsExtractor.class);
+    }
     
     public Set<String> getRequiredParametersForControllerAction(Class<GrailsClass> controllerClass, String actionName) {
 
@@ -14,9 +21,9 @@ public class RequiredParamsExtractor {
         
         for (Method method : controllerClass.getMethods()) {
             if (method.getName().equals(actionName)) {
-                System.out.println("found method!");
                 
                 if (method.isAnnotationPresent(RequiredParameters.class)) {
+                    log.debug("Annotation is present, return value()");
                     for (String string : method.getAnnotation(RequiredParameters.class).value()) {
                         resultSet.add(string);
                     }
