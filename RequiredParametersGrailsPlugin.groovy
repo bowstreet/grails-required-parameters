39989b2256f092a0de93d499e08fa0fb580e955e
Burt Beckwith <burt@burtbeckwith.com>
2013-05-12 00:03:49 -0400

    cleanup

--- RequiredParametersGrailsPlugin.groovy
+++ RequiredParametersGrailsPlugin.groovy
@@ -1,42 +1,15 @@
 class RequiredParametersGrailsPlugin {
-    
+
     def version = "1.0.0-SNAPSHOT"
     def grailsVersion = "2.0 > *"
-    def dependsOn = [:]
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
 
     def title = "Grails Required Parameters Plugin"
     def author = "Matthias Wenz"
     def authorEmail = "mat@bowstreet.de"
-    def description = '''\
-Plugin that enables required parameters for controller actions using simple annotations.
-'''
+    def description = 'Enables required parameters for controller actions using simple annotations.'
 
     def documentation = "https://github.com/bowstreet/grails-required-parameters/"
     def license = "APACHE"
     def organization = [ name: "Bowstreet", url: "http://www.bowstreet.de/" ]
     def scm = [ url: "https://github.com/bowstreet/grails-required-parameters/" ]
-
-    def doWithWebDescriptor = { xml ->
-    }
-
-    def doWithSpring = {
-    }
-
-    def doWithDynamicMethods = { ctx ->
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-    }
-
-    def onChange = { event ->
-    }
-
-    def onConfigChange = { event ->
-    }
-
-    def onShutdown = { event ->
-    }
 }
--- src/java/de/bowstreet/grails/plugins/RequiredParamsExtractor.java
+++ src/java/de/bowstreet/grails/plugins/RequiredParamsExtractor.java
@@ -9,20 +9,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequiredParamsExtractor {
-    
-    public static final Logger log;
-    
-    static {
-        log = LoggerFactory.getLogger(RequiredParamsExtractor.class);
-    }
-    
+
+    public static final Logger log = LoggerFactory.getLogger(RequiredParamsExtractor.class);
+
     public Set<String> getRequiredParametersForControllerAction(Class<GrailsClass> controllerClass, String actionName) {
 
         Set<String> resultSet = new HashSet<String>();
-        
+
         for (Method method : controllerClass.getMethods()) {
             if (method.getName().equals(actionName)) {
-                
+
                 if (method.isAnnotationPresent(RequiredParameters.class)) {
                     log.debug("Annotation is present, return value()");
                     for (String string : method.getAnnotation(RequiredParameters.class).value()) {
@@ -32,8 +28,7 @@ public class RequiredParamsExtractor {
                 break;
             }
         }
-        
+
         return resultSet;
     }
-    
 }
